81e37a15f275ea893f1464f1791bf7a9abee17d0
narthanan24 <narthanan19951124@gmail.com>
2021-06-23 17:47:30 +0530

    All Movies

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import './App.css';
 import Movie from "./Movie";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Movie
-          year={1999}
-          genre={"Sci-Fi"}
-          title={"Matrix"}
-          description={`A Computer hacker learns from mysterious rebels about the true nature of his 
-          reality and his role in the war against its controllers`}
-      />
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                {
+                    id: 1, year: 1999, genre: "Sci-Fi", title: "Matrix",
+                    description: "A computer hacker learns about his reality"
+                },
+                {
+                    id: 2, year: 1994, genre: "Drama", title: "The Shawshank Redemption",
+                    description: "Two imprisoned men bond over a number of years"
+                }
+            ]
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.movies?.map((movie, index) =>
+                    <div key={index}>
+                        <Movie
+                            year={movie.year}
+                            genre={movie.genre}
+                            title={movie.title}
+                            description={movie.description}
+                        />
+                    </div>
+                )
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
