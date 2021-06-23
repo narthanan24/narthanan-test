f35a28c9739ae398bb63f477a06a9748f498d880
narthanan24 <narthanan19951124@gmail.com>
2021-06-23 17:53:21 +0530

    Made Movie a funtional component

--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-class Movie extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>{this.props.year}-{this.props.genre}-{this.props.title}</h1>
-                <h2>{this.props.description}</h2>
-            </div>
-        );
-    }
+const Movie = ({year,title,description,genre}) => {
+    return (
+        <div>
+            <h1>{year}-{genre}-{title}</h1>
+            <h2>{description}</h2>
+        </div>
+    );
 }
 
 export default Movie;
